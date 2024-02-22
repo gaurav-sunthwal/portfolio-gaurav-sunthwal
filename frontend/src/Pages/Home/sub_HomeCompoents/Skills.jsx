f1682e66bfc5393b1a72a1e13177a7c3507596a7
Gaurav Naresh Sunthwal <70788155+codewithgaurav252005@users.noreply.github.com>
2024-02-22 22:01:11 +0530

    df

--- frontend/src/Pages/Home/sub_HomeCompoents/Skills.jsx
+++ frontend/src/Pages/Home/sub_HomeCompoents/Skills.jsx
@@ -8,6 +8,8 @@ import {
   useMediaQuery,
 } from "@chakra-ui/react";
 import codingImg from "/src/assets/Img/image.png";
+import ReactImg from "/src/assets/Img/Skills/react.png";
+import NextJs from "/src/assets/Img/Skills/nextjs.png";
 function Skills() {
   const [isLargerThan] = useMediaQuery("(min-width: 1000px)");
   return (
@@ -22,11 +24,13 @@ function Skills() {
         <HStack
           w={isLargerThan ? "100%" : "100%"}
           flexWrap={isLargerThan ? "nowrap" : "wrap"}
+          justifyContent={"space-between"}
         >
           <Box
             maxW={isLargerThan ? "50%" : "100%"}
             display={"block"}
             m={"auto"}
+            p={5}
           >
             <Image w={isLargerThan ? "100%" : "95%"} src={codingImg} />
           </Box>
@@ -34,6 +38,7 @@ function Skills() {
             <HStack
               overflow={"auto"}
               flexWrap={isLargerThan ? "wrap" : "nowrap"}
+              justifyContent={"center"}
             >
               <SkillIcons
                 imgUrl={
@@ -41,31 +46,15 @@ function Skills() {
                 }
                 skillName={"Python"}
               />
-              
-              <SkillIcons
-                imgUrl={
-                  "https://s3.dualstack.us-east-2.amazonaws.com/pythondotorg-assets/media/community/logos/python-logo-only.png"
-                }
-                skillName={"Python"}
-              />
-              <SkillIcons
-                imgUrl={
-                  "https://s3.dualstack.us-east-2.amazonaws.com/pythondotorg-assets/media/community/logos/python-logo-only.png"
-                }
-                skillName={"Python"}
-              />
-              <SkillIcons
-                imgUrl={
-                  "https://s3.dualstack.us-east-2.amazonaws.com/pythondotorg-assets/media/community/logos/python-logo-only.png"
-                }
-                skillName={"Python"}
-              />
-              <SkillIcons
-                imgUrl={
-                  "https://s3.dualstack.us-east-2.amazonaws.com/pythondotorg-assets/media/community/logos/python-logo-only.png"
-                }
-                skillName={"Python"}
-              />
+
+              <SkillIcons imgUrl={ReactImg} skillName={"React Js"} />
+              <SkillIcons imgUrl={"https://w7.pngwing.com/pngs/87/586/png-transparent-next-js-hd-logo.png"} skillName={"Next Js"} />
+              <SkillIcons imgUrl={ReactImg} skillName={"React Js"} />
+              <SkillIcons imgUrl={ReactImg} skillName={"React Js"} />
+              <SkillIcons imgUrl={ReactImg} skillName={"React Js"} />
+              <SkillIcons imgUrl={ReactImg} skillName={"React Js"} />
+              <SkillIcons imgUrl={ReactImg} skillName={"React Js"} />
+              <SkillIcons imgUrl={ReactImg} skillName={"React Js"} />
             </HStack>
           </Box>
         </HStack>
@@ -77,8 +66,12 @@ function SkillIcons({ imgUrl, skillName }) {
   return (
     <>
       <VStack m={5} p={2}>
-        <Image w={"100px"} src={imgUrl} />
-        <Text>{skillName}</Text>
+        <Box p={2} h={"120px"}>
+          <Image w={"100px"} src={imgUrl} />
+        </Box>
+        <Heading size={"md"} m={3}>
+          {skillName}
+        </Heading>
       </VStack>
     </>
   );
